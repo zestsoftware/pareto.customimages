e1c2abdd75928d46cd7cece01e8c38bb10911fc5
Maurits van Rees <maurits@vanrees.org>
2014-01-23 14:34:29 +0100

    Improve setup.py.

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@ version = '1.0'
 
 setup(name='pareto.customimages',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.rst").read(),
+      description="Custom images control panel to override the ones in a design",
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.rst").read()),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          ],
+      keywords='custom images',
+      author='Thijs Jonkman and Zest Software',
+      author_email='info@zestsoftware.nl',
+      url='https://github.com/zestsoftware/pareto.customimages',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['pareto'],
@@ -24,7 +26,6 @@ setup(name='pareto.customimages',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
       # -*- Entry points: -*-
